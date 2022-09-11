dc4fd30975b28d24871179dd35bf379537e76a0f
kangsudal_notebook <kangsudal@gmail.com>
2022-09-11 13:04:39 +0900

    change layout

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -11,92 +11,93 @@ class Bottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: SizedBox.expand(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              //Today Title
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  FittedBox(
-                    fit: BoxFit.scaleDown,
-                    child: Text(
-                      'Today',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  FittedBox(
-                    fit: BoxFit.scaleDown,
-                      child:Text(
-                    '동, 구',
-                  )),
-                ],
-              ),
-              //Time & Icon & celcius data Row
-              FutureBuilder<List<Forecast>?>(
-                  future: fetchForecastData(),
-                  builder: (context, snapshot) {
-                    try {
-                      if (snapshot.hasData) {
-                        DateTime dt0 = DateTime.fromMillisecondsSinceEpoch(
-                            snapshot.data![0].dt * 1000);
-                        var format_dt0 = DateFormat('HH a').format(dt0);
-                        DateTime dt1 = DateTime.fromMillisecondsSinceEpoch(
-                            snapshot.data![1].dt * 1000);
-                        var format_dt1 = DateFormat('HH a').format(dt1);
-                        DateTime dt2 = DateTime.fromMillisecondsSinceEpoch(
-                            snapshot.data![2].dt * 1000);
-                        var format_dt2 = DateFormat('HH a').format(dt2);
-                        DateTime dt3 = DateTime.fromMillisecondsSinceEpoch(
-                            snapshot.data![3].dt * 1000);
-                        var format_dt3 = DateFormat('HH a').format(dt3);
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: FutureBuilder<List<Forecast>?>(
+            future: fetchForecastData(),
+            builder: (context, snapshot) {
+              try {
+                if (snapshot.hasData) {
+                  DateTime dt0 = DateTime.fromMillisecondsSinceEpoch(
+                      snapshot.data![0].dt * 1000);
+                  var format_dt0 = DateFormat('HH a').format(dt0);
+                  DateTime dt1 = DateTime.fromMillisecondsSinceEpoch(
+                      snapshot.data![1].dt * 1000);
+                  var format_dt1 = DateFormat('HH a').format(dt1);
+                  DateTime dt2 = DateTime.fromMillisecondsSinceEpoch(
+                      snapshot.data![2].dt * 1000);
+                  var format_dt2 = DateFormat('HH a').format(dt2);
+                  DateTime dt3 = DateTime.fromMillisecondsSinceEpoch(
+                      snapshot.data![3].dt * 1000);
+                  var format_dt3 = DateFormat('HH a').format(dt3);
 
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            //One cell widget
-                            CellWidget(
-                              date: '${format_dt0}',
-                              icon: Image.network(
-                                snapshot.data![0].icon,fit: BoxFit.scaleDown,
-                              ),
-                              celcious: '${snapshot.data![0].temp}',
+                  return LayoutBuilder(builder: (context, constraint) {
+                    var height = constraint.maxHeight;
+                    var width = constraint.maxWidth;
+                    print('height:$height, width:$width');
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //One cell widget
+                        Container(
+                          // color: Colors.yellow,
+                          height: height,
+                          width: width / 4,
+                          child: CellWidget(
+                            date: '${format_dt0}',
+                            icon: Image.network(
+                              snapshot.data![0].icon,
+                              fit: BoxFit.scaleDown,
                             ),
-                            CellWidget(
-                              date: '${format_dt1}',
-                              icon: Image.network(snapshot.data![1].icon,fit: BoxFit.scaleDown,),
-                              celcious: '${snapshot.data![1].temp}',
+                            celcious: '${snapshot.data![0].temp}',
+                          ),
+                        ),
+                        Container(
+                          // color: Colors.blueGrey,
+                          height: height,
+                          width: width / 4,
+                          child: CellWidget(
+                            date: '${format_dt1}',
+                            icon: Image.network(
+                              snapshot.data![1].icon,
+                              fit: BoxFit.scaleDown,
                             ),
-                            CellWidget(
-                              date: '${format_dt2}',
-                              icon: Image.network(snapshot.data![2].icon,fit: BoxFit.scaleDown),
-                              celcious: '${snapshot.data![2].temp}',
-                            ),
-                            CellWidget(
-                              date: '${format_dt3}',
-                              icon: Image.network(snapshot.data![3].icon,fit: BoxFit.scaleDown),
-                              celcious: '${snapshot.data![3].temp}',
-                            ),
-                          ],
-                        );
-                      }
-                    } catch (e) {
-                      print('bottom-FutureBuilder: $e');
-                      return Container(
-                        child: Text('데이터를 불러오지 못했습니다.'),
-                      );
-                    }
-                    return Center(child: const CircularProgressIndicator());
-                  }),
-            ],
-          ),
-        ),
+                            celcious: '${snapshot.data![1].temp}',
+                          ),
+                        ),
+                        Container(
+                          height: height,
+                          width: width / 4,
+                          child: CellWidget(
+                            date: '${format_dt2}',
+                            icon: Image.network(snapshot.data![2].icon,
+                                fit: BoxFit.scaleDown),
+                            celcious: '${snapshot.data![2].temp}',
+                          ),
+                        ),
+                        Container(
+                          // color: Colors.orange,
+                          height: height,
+                          width: width / 4,
+                          child: CellWidget(
+                            date: '${format_dt3}',
+                            icon: Image.network(snapshot.data![3].icon,
+                                fit: BoxFit.scaleDown),
+                            celcious: '${snapshot.data![3].temp}',
+                          ),
+                        ),
+                      ],
+                    );
+                  });
+                }
+              } catch (e) {
+                print('bottom-FutureBuilder: $e');
+                return Container(
+                  child: Text('데이터를 불러오지 못했습니다.'),
+                );
+              }
+              return Center(child: const CircularProgressIndicator());
+            }),
       ),
     );
   }
@@ -115,23 +116,12 @@ class CellWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        children: [
-          FittedBox(
-            child: Text(date),
-            fit: BoxFit.scaleDown,
-          ),
-          FittedBox(
-            child: icon,
-            fit: BoxFit.scaleDown,
-          ),
-          FittedBox(
-            child: Text('$celcious°'),
-            fit: BoxFit.scaleDown,
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        Text(date),
+        icon,
+        Text('$celcious°'),
+      ],
     );
   }
 }
